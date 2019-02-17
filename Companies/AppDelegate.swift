d08e936cd593a8a47c28363cafaf0eb65dc2d2f0
Isai Yepez Gómez <isaiyepez@gmail.com>
2019-02-17 16:10:21 -0500

    UI Stylized

--- Companies/AppDelegate.swift
+++ Companies/AppDelegate.swift
@@ -9,6 +9,12 @@
 import UIKit
 import CoreData
 
+class CustomNavigationController: UINavigationController {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -17,6 +23,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        
+        let companiesController = ViewController()
+        let navController = CustomNavigationController(rootViewController: companiesController)
+        window?.rootViewController = navController
+        
         return true
     }
 
--- Companies/ViewController.swift
+++ Companies/ViewController.swift
@@ -12,9 +12,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.title = "Companies"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named:"plus"), style: .plain, target: self, action: #selector(handleAddCompany))
+        
+        //view.backgroundColor = .blue
+        //navigationController?.navigationBar.backgroundColor = .red
+        setupNavigationStyle()
     }
-
-
+    
+    @objc func handleAddCompany() {
+        print("company added")
+    
+    }
+    
+    func setupNavigationStyle(){
+    
+        navigationController?.navigationBar.isTranslucent = false
+        let lightred = UIColor(displayP3Red: 247/255, green: 66/255, blue: 82/255, alpha: 1)
+        navigationController?.navigationBar.barTintColor = lightred
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+    }
+    
 }
 
